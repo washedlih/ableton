094286f0a2294ac5b7b25a7858178da0d9c3ac35
washedlih <khanglee11@gmail.com>
2023-01-11 23:04:48 -0500

    added nextjs font

--- pages/_app.js
+++ pages/_app.js
@@ -1,5 +1,15 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import { Jost } from "@next/font/google";
+
+const jost = Jost({
+  subsets: ["latin"],
+  weight: ["400"],
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <main className={jost.className}>
+      <Component {...pageProps} />
+    </main>
+  );
 }
